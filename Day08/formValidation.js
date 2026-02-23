a655a97a20362d8a039feb00feed0ae4a42ec44b
Harshada Keste <kesteharshada87@gmail.com>
2026-02-23 21:50:40 +0530

    Form Validation

---
+++ Day08/formValidation.js
@@ -0,0 +1,4 @@
+//documents obj manupulation---show error msg--changing html using JS--EX--display success msg
+//show error msg,change color to other
+//document obj model--DOM---convert html page into obj
+//.value in DOM used to take input from user
